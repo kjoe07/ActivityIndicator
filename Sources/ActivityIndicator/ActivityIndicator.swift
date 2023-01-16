68024d734ddc4de8b545d31638c6ecb938a55b1e
Yoel Jimenez del Valle <yoel07@gmail.com>
2023-01-16 13:27:28 -0500

    fix constrains

--- Sources/ActivityIndicator/ActivityIndicator.swift
+++ Sources/ActivityIndicator/ActivityIndicator.swift
@@ -47,10 +47,20 @@ public extension UIViewController{
         NSLayoutConstraint.activate([
             activityIndicator.centerXAnchor.constraint(equalTo: loadingView.centerXAnchor),
             activityIndicator.centerYAnchor.constraint(equalTo: loadingView.centerYAnchor),
+            activityIndicator.heightAnchor.constraint(equalToConstant: 40),
+            activityIndicator.widthAnchor.constraint(equalToConstant: 40),
             loadingView.centerYAnchor.constraint(equalTo: container.centerYAnchor),
             loadingView.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+            loadingView.heightAnchor.constraint(equalToConstant: 80),
+            loadingView.widthAnchor.constraint(equalToConstant: 80),
+            loadingView.topAnchor.constraint(equalTo: container.topAnchor),
+            loadingView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            loadingView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            loadingView.bottomAnchor.constraint(equalTo: container.bottomAnchor),
             container.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            container.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+            container.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            container.widthAnchor.constraint(equalToConstant: 80),
+            container.heightAnchor.constraint(equalToConstant: 80)
         ])
         activityIndicator.startAnimating()
     }
